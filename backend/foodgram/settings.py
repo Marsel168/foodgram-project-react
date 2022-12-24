f452ad68a38f3564ce3c5aad70f35ec461adcea5
Marsel <marsel168@bk.ru>
2022-12-24 11:33:04 +0300

    fix Dockerfile

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -102,6 +102,12 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 AUTH_USER_MODEL = 'users.User'
 
